23ffaec658d14a6934bb5c0df11702a2bcd3ef2a
DR <hello@rstat.org>
2018-11-17 05:01:27 +0000

    serviceId support

--- cookiesync/struct.py
+++ cookiesync/struct.py
@@ -1,5 +1,17 @@
 from prodict import Prodict
 from aioredis.pool import ConnectionsPool
+from collections import namedtuple
+
+
+class ServiceId(namedtuple("ServiceId", ["service", "id"])):
+    __slots__ = ()
+
+    def __str__(self):
+        return self.service + ':' + str(self.id)
+
+    @classmethod
+    def from_str(cls, value):
+        return cls(*value.split(':'))
 
 
 class Partner:
